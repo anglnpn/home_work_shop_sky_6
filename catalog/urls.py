04dab0a497d2cddf59744d23c343086825c84f72
angelina <angln.pnn@gmail.com>
2023-11-12 14:08:52 -0300

    1) создано приложение catalog 2) реализованы 3 шаблона для главной страницы, контактной информации и категорий товаров 3) реализованы 3 контролера за отображения главной стр, контактной стр и категории стр

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from catalog.views import index, contacts, categories
+
+urlpatterns = [
+    path('', index,  name='index'),
+    path('contacts/', contacts,  name='contacts'),
+    path('categories/', categories,  name='categories')
+]
---
+++ catalog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def contacts(request):
+    """
+    Функция получает данные, введенные пользователем
+    """
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} {phone}: {message}')
+    return render(request, 'main/contacts.html')
+
+
+def categories(request):
+    return render(request, 'main/categories.html')
